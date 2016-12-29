ca88cc855c348781d52120eb3f367597898b36b8
naumvd95 <naumvd95@gmail.com>
2016-12-29 10:21:59 +0300

    refactoring, bug-fix, check auto trigger_update
    auth.env: env-vars replaced into security file
    
    app.py: DELAY decreased to default (there is troubles if value ==10)
    code style changes to improve visibility in slack messages
    
    compose: decrease compose version
    fix unreadable(for compose) bash-defining
    remove strange volume-define
    NEED TO DISCUSS ALL CASES

--- app/app.py
+++ app/app.py
@@ -8,21 +8,13 @@ SLACK_BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 BOT_NAME = os.environ.get('BOT_NAME')
 JENKINS_URL = os.environ.get('JENKINS_URL')
 JOB_TOKEN = os.environ.get('JOB_TOKEN')
-READ_WEBSOCKET_DELAY = 10
 
 # get bot credentials based on slack_bot_token
 JOB_RUN_URL = '{jenkins_url}/view/utils/job/update_jobs/buildWithParameters?token={job_token}'.format(
     job_token=JOB_TOKEN, jenkins_url=JENKINS_URL)
 JOB_LAST_BUILD = '{jenkins_url}/view/utils/job/update_jobs/lastBuild/'.format(jenkins_url=JENKINS_URL)
-BOT_ID = None
-
-# constants
-UPDATE_COMMAND = "update"
-AUTO_UPDATE_COMMAND = "trigger_update"
-HELP_COMMAND = "help"
 
 slack_client = SlackClient(SLACK_BOT_TOKEN)
-
 
 def auth(bot_name):
     api_call = slack_client.api_call("users.list")
@@ -35,6 +27,13 @@ def auth(bot_name):
     else:
         print("could not find bot user with the name " + BOT_NAME)
 
+# constants
+UPDATE_COMMAND = "update"
+AUTO_UPDATE_COMMAND = "trigger_update"
+HELP_COMMAND = "help"
+BOT_ID = auth(bot_name=BOT_NAME)
+AT_BOT = "<@" + BOT_ID + ">"
+
 
 def handle_command(command, channel):
     """
@@ -42,7 +41,7 @@ def handle_command(command, channel):
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    response = "Go to hell with all your blankety! use * {help_command} * command!".format(help_command=HELP_COMMAND)
+    response = "Go to hell with all your blankety! use *{help_command}* command!".format(help_command=HELP_COMMAND)
 
     if command.startswith(HELP_COMMAND):
         response = "Thousand devils!...write some more code then I can do that!"
@@ -50,16 +49,16 @@ def handle_command(command, channel):
     if command.startswith(UPDATE_COMMAND):
         resp = get(JOB_RUN_URL)
         if resp.status_code < 300:
-            response = "I swear ass octopus that my guys will update your"
-            "jobs before sunrise! Watch the state, Cap:{job_last_build}".format(job_last_build=JOB_LAST_BUILD)
+            response = "I swear ass octopus that my guys will update your\
+            jobs before sunrise! Watch the state, Cap:{job_last_build}".format(job_last_build=JOB_LAST_BUILD)
         else:
             response = "Something went wrong"
 
     if AUTO_UPDATE_COMMAND in command:
         resp = get(JOB_RUN_URL)
         if resp.status_code < 300:
-            response = "Take a rest Cap, I will take a command and update all jobs!"
-            "Watch the state, Cap:{job_last_build}".format(job_last_build=JOB_LAST_BUILD)
+            response = "Take a rest Cap, I will take a command and update all jobs!\
+            Watch the state, Cap:{job_last_build}".format(job_last_build=JOB_LAST_BUILD)
         else:
             response = "Something went wrong"
 
@@ -93,8 +92,7 @@ def parse_slack_output(slack_rtm_output):
 
 
 if __name__ == "__main__":
-    BOT_ID = auth(bot_name=BOT_NAME)
-    AT_BOT = "<@" + BOT_ID + ">"
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("Captain Jenkins up and running!")
         while True:
